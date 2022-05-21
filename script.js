e26a1a3f08a958a8bb3d20ec5b9fda64288b248b
David Enikuomehin <davidenikuomehin1@gmail.com>
2022-05-21 12:18:52 +0100

    peform operation based on user input

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-//calculate basic arithmetic operations
+// global variables
+const operator = prompt("What operator will you be using?)");
+const num1 = parseInt(prompt("1st Number: "));
+const num2 = parseInt(prompt("2nd Number: "));
+
+//function calculating basic arithmetic operations
 function add(num1, num2) {
     let sum = num1 + num2;
     return sum;
@@ -18,3 +23,12 @@ function divide(num1, num2) {
     let div = num1 / num2;
     return div; 
 }
+
+//performs operation based on arithmetic operator selected 
+function operate(num1, num2) {
+    if (operator == "+") {
+        add(num1, num2);
+        console.log(add(num1, num2));
+    }
+}
+operate(num1, num2);
