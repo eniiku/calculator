1659e8db1856a90b409d845165acdc926f8c06a5
David Enikuomehin <davidenikuomehin1@gmail.com>
2022-05-21 11:14:02 +0100

    perform basic arithmethic

---
+++ script.js
@@ -0,0 +1,20 @@
+//calculate basic arithmetic operations
+function add(num1, num2) {
+    let sum = num1 + num2;
+    return sum;
+}
+
+function subtract(num1, num2) {
+    let minus = num1 - num2;
+    return minus; 
+}
+
+function multiply(num1, num2) {
+    let mul = num1 * num2;
+    return mul; 
+}
+
+function divide(num1, num2) {
+    let div = num1 / num2;
+    return div; 
+}
