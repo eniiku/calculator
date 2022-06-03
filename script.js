b71cf003299f98733a68751c641cbb0152b6fe60
David Enikuomehin <davidenikuomehin1@gmail.com>
2022-06-03 04:42:12 +0100

    update script <unknown>

--- script.js
+++ script.js
@@ -1,34 +1,52 @@
-// global variables
-const operator = prompt("What operator will you be using?)");
-const num1 = parseInt(prompt("1st Number: "));
-const num2 = parseInt(prompt("2nd Number: "));
-
 //function calculating basic arithmetic operations
 function add(num1, num2) {
-    let sum = num1 + num2;
-    return sum;
+  let sum = num1 + num2;
+  return sum;
 }
 
 function subtract(num1, num2) {
-    let minus = num1 - num2;
-    return minus; 
+  let minus = num1 - num2;
+  return minus; 
 }
 
 function multiply(num1, num2) {
-    let mul = num1 * num2;
-    return mul; 
+  let mul = num1 * num2;
+  return mul; 
 }
 
 function divide(num1, num2) {
-    let div = num1 / num2;
-    return div; 
+  let div = num1 / num2;
+  return div; 
+}
+
+function remainder(num1, num2) {
+  let rem = num1 % num2;
+  return rem
 }
 
 //performs operation based on arithmetic operator selected 
 function operate(num1, num2) {
-    if (operator == "+") {
-        add(num1, num2);
-        console.log(add(num1, num2));
-    }
+  if (operator == "+") {
+    add(num1, num2);
+    console.log(add(num1, num2));
+  } else if (operator == "-") {
+    subtract(num1, num2);
+    console.log(add(num1, num2));
+  } else if (operator == "*") {
+    multiply(num1, num2);
+    console.log(add(num1, num2));
+  } else if (operator == "/") {
+    divide(num1, num2);
+    console.log(add(num1, num2));
+  } else if (operator == "+") {
+    remainder(num1, num2);
+    console.log(add(num1, num2));
+  }
 }
-operate(num1, num2);
+const mainDisplay = document.querySelector('main-display')
+const numberButtons = document.querySelectorAll('[data-number]');
+numberButtons.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    mainDisplay.append(e.target.textContent);
+  });
+});
